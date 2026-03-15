ebe7f85612d816342ada735ca2d716329e7c2ce4
Batoul Allahham <batoolallahham5@gmail.com>
2026-03-15 17:24:45 +0300

    Added create, retrieve, update, partial_update and destroy methods

--- course_project_api/views.py
+++ course_project_api/views.py
@@ -46,6 +46,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API View Set"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
@@ -54,4 +55,29 @@ class HelloViewSet(viewsets.ViewSet):
             'Automatically maps to URLs using Routers',
             'Provides more functionality with less code'
         ]
-        return Response({'message':'Hello', 'a_viewset':a_viewset})
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello massage"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello and welcome {name} !'
+            return Response({'message': message})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):  # it retrieves an object based on it primary key
+        """Handle getting the object based on the pk"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object like the http method put"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating the specific part of the object like http method Patch"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle deleting an object using its primary key """
+        return Response({'http_method': 'DELETE'})
